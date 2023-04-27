d87030003fd8b58357ad433d1b8adfd7d339f4ce
mosius27 <mosius27@yandex.ru>
2023-04-27 18:50:03 +0300

    .

---
+++ orm_migrations/school/views.py
@@ -0,0 +1,18 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    students = Student.objects.all()
+
+    template = 'school/students_list.html'
+    context = {'students': students}
+
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
